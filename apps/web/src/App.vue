b72b1fb6bc324775b0dec297a63caefe1e011aba
chensihua <kushiyou@itcast.cn>
2026-02-17 23:42:47 +0800

    2026-2-17 init login global component and add userType in common

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <RouterView />
   <Search/>
+  <Login/>
 </template>
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import Search from './components/Search/index.vue'
+import Login from './components/Login/index.vue'
 </script>  
